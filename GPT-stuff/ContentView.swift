932c2bc8ce325bf59e02e2c4fe485da050d9f002
Sean Okuyama-Smith <sean.o_s@icloud.com>
2023-07-20 09:10:00 +0100

    Initial Commit

---
+++ GPT-stuff/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GPT-stuff
+//
+//  Created by Sean Okuyama-Smith on 20/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GPT-stuff/GPT_stuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPT_stuffApp.swift
+//  GPT-stuff
+//
+//  Created by Sean Okuyama-Smith on 20/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct GPT_stuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
